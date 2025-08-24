4807f8fc672fc0452228d8680d4a47ac45b7fd98
fadelanumah <fadelanumah@gmail.com>
2025-08-25 00:28:31 +0700

    chore(common): fix the based response common

--- common/response.go
+++ common/response.go
@@ -1,25 +1,7 @@
 package common
 
-import "net/http"
-
 type BaseResponse struct {
 	Status  int    `json:"status"`
 	Message string `json:"message"`
 	Data    any    `json:"data"`
-}
-
-func SuccessResponse(message string, data any) BaseResponse {
-	return BaseResponse{
-		Status:  http.StatusOK,
-		Message: message,
-		Data:    data,
-	}
-}
-
-func ErrorResponse(errorStatus int, message string, data any) BaseResponse {
-	return BaseResponse{
-		Status:  http.StatusOK,
-		Message: message,
-		Data:    data,
-	}
 }
