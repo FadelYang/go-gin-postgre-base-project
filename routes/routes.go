295ca9cda80c59914be513826390ea51b58373d0
fadelanumah <fadelanumah@gmail.com>
2025-08-25 20:55:25 +0700

    docs: add swagger config

--- routes/routes.go
+++ routes/routes.go
@@ -6,10 +6,15 @@ import (
 	"github.com/gin-gonic/gin"
 
 	ex "project-root/modules/examples/routes"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func InitRoutes(r *gin.Engine, p *providers.Providers) {
 	api := r.Group("api/v1")
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	ex.RegisterRoutes(api, p.Examples)
 }
