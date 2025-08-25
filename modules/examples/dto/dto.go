c82959feeb4ea27cc2d183cd20e07afca3252c91
fadelanumah <fadelanumah@gmail.com>
2025-08-25 21:38:27 +0700

    docs(example): add struct for create body request

--- modules/examples/dto/dto.go
+++ modules/examples/dto/dto.go
@@ -6,3 +6,7 @@ type ExampleDTO struct {
 	ID   uuid.UUID `json:"id" example:"a53515e3-5a7f-440b-82f6-3d84ac7ce746"`
 	Name string    `json:"name" example:"Bakwan Jagung"`
 }
+
+type CreateExample struct {
+	Name string `json:"name" example:"Bakwan Jagung" binding:"required"`
+}
