0ef590a73e6db5d64dee49a3bc090c577862553c
fadelanumah <fadelanumah@gmail.com>
2025-08-25 21:20:42 +0700

    fix(common): fix BaseResponse to compatible with swaggo

--- common/response.go
+++ common/response.go
@@ -1,7 +1,7 @@
 package common
 
-type BaseResponse struct {
+type BaseResponse[T any] struct {
 	Status  int    `json:"status"`
 	Message string `json:"message"`
-	Data    any    `json:"data"`
+	Data    T      `json:"data"`
 }
