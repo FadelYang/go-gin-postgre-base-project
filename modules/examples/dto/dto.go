0286790a80cb23a2fc60f296a5a6d791292867a3
fadelanumah <fadelanumah@gmail.com>
2025-08-25 21:26:27 +0700

    docs(example): add example value for each field

--- modules/examples/dto/dto.go
+++ modules/examples/dto/dto.go
@@ -3,6 +3,6 @@ package dto
 import "github.com/google/uuid"
 
 type ExampleDTO struct {
-	ID   uuid.UUID `json:"id"`
-	Name string    `json:"name"`
+	ID   uuid.UUID `json:"id" example:"a53515e3-5a7f-440b-82f6-3d84ac7ce746"`
+	Name string    `json:"name" example:"Bakwan Jagung"`
 }
